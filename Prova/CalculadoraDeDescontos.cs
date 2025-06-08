3ec0edca18509f2b31522e75817052c838849d5b
JoaoVitor1709 <joao.resende@sou.unifeb.edu.br>
2025-06-08 18:15:35 -0300

    Adiciona CalculadoraDeDescontos com suporte a múltiplas estratégias

---
+++ Prova/CalculadoraDeDescontos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    public class CalculadoraDeDescontos
+    {
+        private readonly List<IDescontoStrategy> _estrategias;
+
+        public CalculadoraDeDescontos(List<IDescontoStrategy> estrategias)
+        {
+            _estrategias = estrategias;
+        }
+
+        public decimal CalcularDescontoTotal(Pedido pedido)
+        {
+            decimal descontoTotal = 0;
+
+            foreach (var item in pedido.Itens)
+            {
+                foreach (var estrategia in _estrategias)
+                {
+                    descontoTotal += estrategia.CalcularDesconto(item);
+                }
+            }
+
+            return descontoTotal;
+        }
+    }
+}
