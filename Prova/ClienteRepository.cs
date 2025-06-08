86d3385778a2672991c21ec86415c1bc12be11be
JoaoVitor1709 <joao.resende@sou.unifeb.edu.br>
2025-06-08 16:42:49 -0300

    Adiciona repositórios específicos: ProdutoRepository, ClienteRepository, PedidoRepository

---
+++ Prova/ClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    public class ClienteRepository : InMemoryRepository<Cliente>
+    {
+        public ClienteRepository() : base(cliente => cliente.Id) { }
+    }
+}
---
+++ Prova/ProdutoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    public class ProdutoRepository : InMemoryRepository<Produto>
+    {
+        public ProdutoRepository() : base(produto => produto.Id) { }
+    }
+}
