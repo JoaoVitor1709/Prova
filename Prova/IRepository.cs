920830c8b4215e3ef62138f630de928a7b4182f0
JoaoVitor1709 <joao.resende@sou.unifeb.edu.br>
2025-06-08 16:25:49 -0300

    Cria interface IRepository<T> e implementação genérica InMemoryRepository

---
+++ Prova/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    public interface IRepository<T>
+    {
+        void Adicionar(T entidade);
+        T ObterPorId(int id);
+        IEnumerable<T> ObterTodos();
+    }
+}
---
+++ Prova/InMemoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly List<T> _dados = new();
+        protected readonly Func<T, int> _getId;
+
+        public InMemoryRepository(Func<T, int> getId)
+        {
+            _getId = getId;
+        }
+
+        public void Adicionar(T entidade)
+        {
+            _dados.Add(entidade);
+        }
+
+        public T ObterPorId(int id)
+        {
+            return _dados.FirstOrDefault(e => _getId(e) == id);
+        }
+
+        public IEnumerable<T> ObterTodos()
+        {
+            return _dados;
+        }
+    }
+}
