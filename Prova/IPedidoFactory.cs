66a063d9bf3e302c67b2c0139683a510c9923a27
JoaoVitor1709 <joao.resende@sou.unifeb.edu.br>
2025-06-08 17:21:34 -0300

    Implementa PedidoFactory para construir pedidos com itens e cliente

---
+++ Prova/IPedidoFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    public interface IPedidoFactory
+    {
+        Pedido CriarPedido(int idPedido, Cliente cliente, List<(Produto produto, int quantidade)> itens);
+    }
+}
---
+++ Prova/PedidoFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    public class PedidoFactory : IPedidoFactory
+    {
+        public Pedido CriarPedido(int idPedido, Cliente cliente, List<(Produto produto, int quantidade)> itens)
+        {
+            if (cliente == null) throw new ArgumentNullException(nameof(cliente));
+            if (itens == null || !itens.Any()) throw new ArgumentException("Lista de itens inválida.");
+
+            var pedido = new Pedido(idPedido, cliente);
+
+            foreach (var (produto, quantidade) in itens)
+            {
+                var item = new ItemPedido(produto, quantidade);
+                pedido.AdicionarItem(item);
+            }
+
+            return pedido;
+        }
+    }
+}
